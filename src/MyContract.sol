19eab2b18da419336012d8dad9bbb4dd8d940410
Jimbo Liao <fong.milk@gmail.com>
2023-04-19 14:38:30 +0800

    format

--- src/MyContract.sol
+++ src/MyContract.sol
@@ -5,18 +5,17 @@ pragma solidity ^0.8.9;
 // import "hardhat/console.sol";
 
 contract MyContract {
+    address public user1;
+    address public user2;
 
-  address public user1;
-  address public user2;
+    event Receive(address from, uint256 amount);
 
-  event Receive(address from, uint256 amount);
+    constructor(address _user1, address _user2) {
+        user1 = _user1;
+        user2 = _user2;
+    }
 
-  constructor(address _user1, address _user2) {
-    user1 = _user1;
-    user2 = _user2;
-  }
-
-  receive() external payable {
-    emit Receive(msg.sender, msg.value);
-  }
+    receive() external payable {
+        emit Receive(msg.sender, msg.value);
+    }
 }
--- test/MyContract.t.sol
+++ test/MyContract.t.sol
@@ -2,49 +2,48 @@
 pragma solidity ^0.8.9;
 
 import "forge-std/Test.sol";
-import { MyContract } from "../src/MyContract.sol";
+import {MyContract} from "../src/MyContract.sol";
 
 contract MyContractTest is Test {
+    MyContract instance;
+    address user1;
+    address user2;
 
-  MyContract instance;
-  address user1;
-  address user2;
+    event Receive(address from, uint256 amount);
 
-  event Receive(address from, uint256 amount);
+    function setUp() public {
+        //  Set user1, user2
+        user1 = address(1);
+        user2 = address(2);
+        // (optional) label user1 as bob, user2 as alice
+        vm.label(user1, "bob");
+        vm.label(user2, "alice");
+        //  Create a new instance of MyContract
+        instance = new MyContract(user1, user2);
+    }
 
-  function setUp() public {   
-    //  Set user1, user2 
-    user1 = address(1);
-    user2 = address(2);
-    // (optional) label user1 as bob, user2 as alice
-    vm.label(user1,"bob");
-    vm.label(user2,"alice");
-    //  Create a new instance of MyContract
-    instance = new MyContract(user1, user2);
-  }
+    function testConstructor() public {
+        // Assert instance.user1() is user1
+        // Assert instance.user2() is user2
+        assertEq(instance.user1(), user1);
+        assertEq(instance.user2(), user2);
+    }
 
-  function testConstructor() public {
-    // Assert instance.user1() is user1
-    // Assert instance.user2() is user2
-    assertEq(instance.user1(), user1);
-    assertEq(instance.user2(), user2);
-  }
-
-  function testReceiveEther() public {
-    // expect event
-    vm.expectEmit(false,false,false,true,address(instance));
-    // expected specific log
-    emit Receive(user1, 1 ether);
-    // pretending you are user1
-    vm.startPrank(user1);
-    // let user1 have 1 ether
-    vm.deal(user1, 1 ether);
-    // send 1 ether to instance
-    (bool success, ) = address(instance).call{value: 1 ether}(""); // send 1 ether to instance
-    require(success);
-    // Assert instance has 1 ether in balance
-    assertEq(address(instance).balance, 1 ether);
-    // stop pretending
-    vm.stopPrank();
-  }
+    function testReceiveEther() public {
+        // expect event
+        vm.expectEmit(false, false, false, true, address(instance));
+        // expected specific log
+        emit Receive(user1, 1 ether);
+        // pretending you are user1
+        vm.startPrank(user1);
+        // let user1 have 1 ether
+        vm.deal(user1, 1 ether);
+        // send 1 ether to instance
+        (bool success,) = address(instance).call{value: 1 ether}(""); // send 1 ether to instance
+        require(success);
+        // Assert instance has 1 ether in balance
+        assertEq(address(instance).balance, 1 ether);
+        // stop pretending
+        vm.stopPrank();
+    }
 }
